023d63bcb1f12dc565747f93b502fc28310440a7
ovimax <osilvium@gmail.com>
2018-12-06 01:42:14 +0100

    class viewer
    define the basics of class viewer

--- controller/core/MainController.php
+++ controller/core/MainController.php
@@ -6,17 +6,26 @@ require("ModelController.php");
 
 class MainController
 {
+	function __construct()
+	{
+		//$this->view = new ViewerController();
+		//$this->model = new ModelController();
+	}
+
 	function viewer($template = null, $date = array())
 	{
-		$view = file_get_contents("view/core/template.html");
+		$view = new ViewerController("demos");
+
+		//$footer = $view->get_footer();
+
+		/*$view = file_get_contents("view/core/template.html");
 		$header = file_get_contents("view/core/header.html");
-		$footer = file_get_contents('view/core/footer.html');
 		$body = file_get_contents("view/demo.html");
 		$body = str_replace("<< #mundo# >>","<h1>HOLA MUNDOS</h1>",$body);
 		$view = str_replace("<< #header# >>",$header,$view);
 		$view = str_replace("<< #body# >>",$body,$view);
-		$view = str_replace("<< #footer# >>",$footer,$view);
-		echo $view;
+		$view = str_replace("<< #footer# >>",$footer,$view);*/
+		echo $view->launch_view();
 
 	}
 }
--- controller/core/ViewerController.php
+++ controller/core/ViewerController.php
@@ -1,6 +1,69 @@
 <?php
 
 class ViewerController
-{
-	
+{	
+	protected $header;
+	protected $boody;
+	protected $footer;
+
+	function __construct($view = null)
+	{
+		if($view)
+		{	
+			$this->view = $view;
+			self::launch_view();
+		}
+	}
+
+	function launch_view()
+	{
+		if(self::view_exists())
+		{
+			return self::get_view();
+		}
+		else
+		{
+			die("ERROR 404: File not found");
+		}
+	}
+
+	function view_exists()
+	{
+		if (file_exists('view/'.$this->view.'.html'))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	function get_view()
+	{
+		$view = file_get_contents("view/core/template.html");
+
+		$header = self::get_header();
+		$footer = self::get_footer();
+
+		$body = file_get_contents('view/'.$this->view.'.html');
+		$body = str_replace("<< #mundo# >>","<h1>HOLA MUNDOS</h1>",$body);
+
+		$view = str_replace("<< #header# >>",$header,$view);
+		$view = str_replace("<< #body# >>",$body,$view);
+		$view = str_replace("<< #footer# >>",$footer,$view);
+
+		return $view;
+
+	}
+
+	function get_footer()
+	{
+		$this->footer = file_get_contents('view/core/footer.html');
+		return $this->footer;
+	}
+
+	function get_header()
+	{
+		$this->header = file_get_contents("view/core/header.html");
+		return $this->header;
+	}
 }
